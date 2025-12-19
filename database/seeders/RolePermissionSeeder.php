4bbd7e298f31841234b9931411be556d66157b08
anilmauganj <anil.mauganj100@gmail.com>
2025-12-19 18:54:17 +0530

    feat: add roles and permissions system

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'dashboard',
+            'admin.roles.index',
+            'admin.roles.create',
+            'admin.roles.edit',
+            'admin.permissions.index',
+            'admin.users.index',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        $admin = Role::firstOrCreate(['name' => 'admin']);
+        $admin->syncPermissions(Permission::all());
+    }
+}
